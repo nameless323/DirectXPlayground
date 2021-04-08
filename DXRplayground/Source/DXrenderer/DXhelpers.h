bcda001c820c3a81f02bc491576860c5c4538eea
Aleksandr Vorontcov <nameless323@gmail.com>
2021-04-07 21:14:05 -0400

    Common root sig and pso

--- DXRplayground/Source/DXrenderer/DXhelpers.h
+++ DXRplayground/Source/DXrenderer/DXhelpers.h
@@ -6,6 +6,7 @@
 #include <string>
 #include <DirectXMath.h>
 #include "External/Dx12Helpers/d3dx12.h"
+#include "DXrenderer/RenderContext.h"
 
 namespace DirectxPlayground
 {
@@ -96,5 +97,100 @@ inline std::array<D3D12_INPUT_ELEMENT_DESC, 4>& GetInputLayoutUV_N_T()
         { "TANGENT", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 32, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
     } };
     return layout;
+}
+
+
+constexpr UINT ConstantBuffersCountPerSpace = 8;
+constexpr UINT MaxSpacesForConstantBuffers = 2;
+constexpr UINT TextureTableIndex = ConstantBuffersCountPerSpace * MaxSpacesForConstantBuffers;
+
+inline UINT GetCBRootParamIndex(UINT index, UINT space = 0)
+{
+    assert(space < 2 && index < ConstantBuffersCountPerSpace && "CB space is invalid");
+    return index + space * ConstantBuffersCountPerSpace;
+}
+
+inline HRESULT CreateCommonRootSignature(ID3D12Device* device, REFIID riid, void** ppv)
+{
+    std::vector<CD3DX12_ROOT_PARAMETER1> cbParams;
+    for (UINT i = 0; i < MaxSpacesForConstantBuffers; ++i)
+    {
+        for (UINT j = 0; j < ConstantBuffersCountPerSpace; ++j)
+        {
+            cbParams.emplace_back();
+            cbParams.back().InitAsConstantBufferView(j, i);
+        }
+    }
+
+    D3D12_DESCRIPTOR_RANGE1 texRange;
+    texRange.NumDescriptors = RenderContext::MaxTextures;
+    texRange.BaseShaderRegister = 0;
+    texRange.OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+    texRange.Flags = D3D12_DESCRIPTOR_RANGE_FLAG_DATA_VOLATILE; // Double check perf
+    texRange.RegisterSpace = 0;
+    texRange.RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
+
+    cbParams.emplace_back();
+    cbParams.back().InitAsDescriptorTable(1, &texRange, D3D12_SHADER_VISIBILITY_PIXEL);
+
+    CD3DX12_STATIC_SAMPLER_DESC linearClamp(
+        0,
+        D3D12_FILTER_MIN_MAG_MIP_LINEAR,
+        D3D12_TEXTURE_ADDRESS_MODE_CLAMP,
+        D3D12_TEXTURE_ADDRESS_MODE_CLAMP,
+        D3D12_TEXTURE_ADDRESS_MODE_CLAMP);
+    CD3DX12_STATIC_SAMPLER_DESC linearWrap(
+        1,
+        D3D12_FILTER_MIN_MAG_MIP_LINEAR,
+        D3D12_TEXTURE_ADDRESS_MODE_WRAP,
+        D3D12_TEXTURE_ADDRESS_MODE_WRAP,
+        D3D12_TEXTURE_ADDRESS_MODE_WRAP);
+    std::array<CD3DX12_STATIC_SAMPLER_DESC, 2> staticSamplers = { linearClamp, linearWrap };
+
+    D3D12_FEATURE_DATA_ROOT_SIGNATURE signatureData = {};
+    signatureData.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_1;
+
+    if (FAILED(device->CheckFeatureSupport(D3D12_FEATURE_ROOT_SIGNATURE, &signatureData, sizeof(signatureData))))
+        signatureData.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_0;
+
+    D3D12_ROOT_SIGNATURE_FLAGS flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+    CD3DX12_VERSIONED_ROOT_SIGNATURE_DESC rootSignatureDesc;
+    rootSignatureDesc.Init_1_1(UINT(cbParams.size()), cbParams.data(), UINT(staticSamplers.size()), staticSamplers.data(), flags);
+
+    Microsoft::WRL::ComPtr<ID3DBlob> signature;
+    Microsoft::WRL::ComPtr<ID3DBlob> rootSignatureCreationError;
+    HRESULT hr = D3DX12SerializeVersionedRootSignature(&rootSignatureDesc, signatureData.HighestVersion, &signature, &rootSignatureCreationError);
+    if (hr != S_OK)
+    {
+        OutputDebugStringA(reinterpret_cast<char*>(rootSignatureCreationError->GetBufferPointer()));
+        return hr;
+    }
+    return device->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), riid, ppv);
+}
+
+inline D3D12_GRAPHICS_PIPELINE_STATE_DESC GetDefaultOpaquePsoDescriptor(ID3D12RootSignature* rootSig, UINT numRenderTargets)
+{
+    D3D12_GRAPHICS_PIPELINE_STATE_DESC desc = {};
+    desc.pRootSignature = rootSig;
+    desc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+    desc.RasterizerState.CullMode = D3D12_CULL_MODE_BACK;
+    desc.RasterizerState.FillMode = D3D12_FILL_MODE_SOLID;
+    desc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+
+    D3D12_DEPTH_STENCIL_DESC dsDesc = {};
+    dsDesc.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ALL;
+    dsDesc.DepthEnable = true;
+    dsDesc.StencilEnable = false;
+    dsDesc.DepthFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
+
+    desc.DepthStencilState = dsDesc;
+    desc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+
+    desc.NumRenderTargets = numRenderTargets;
+
+    desc.SampleMask = UINT_MAX;
+    desc.SampleDesc.Count = 1;
+
+    return desc;
 }
 }
--- DXRplayground/Source/Scene/GltfViewer.cpp
+++ DXRplayground/Source/Scene/GltfViewer.cpp
@@ -17,16 +17,6 @@
 namespace DirectxPlayground
 {
 
-static constexpr UINT BuffersPerSpace = 8;
-static constexpr UINT MaxCBSpaces = 2;
-static constexpr UINT TextureTableIndex = BuffersPerSpace * MaxCBSpaces;
-
-UINT GetCBRootParamIndex(UINT index, UINT space = 0)
-{
-    assert(space < 2 && index < BuffersPerSpace && "CB space is invalid");
-    return index + space * BuffersPerSpace;
-}
-
 GltfViewer::~GltfViewer()
 {
     SafeDelete(m_cameraCb);
@@ -133,58 +123,7 @@ void GltfViewer::LoadGeometry(RenderContext& context)
 
 void GltfViewer::CreateRootSignature(RenderContext& context)
 {
-    std::vector<CD3DX12_ROOT_PARAMETER1> cbParams;
-    for (UINT i = 0; i < MaxCBSpaces; ++i)
-    {
-        for (UINT j = 0; j < BuffersPerSpace; ++j)
-        {
-            cbParams.emplace_back();
-            cbParams.back().InitAsConstantBufferView(j, i);
-        }
-    }
-
-    D3D12_DESCRIPTOR_RANGE1 texRange;
-    texRange.NumDescriptors = RenderContext::MaxTextures;
-    texRange.BaseShaderRegister = 0;
-    texRange.OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
-    texRange.Flags = D3D12_DESCRIPTOR_RANGE_FLAG_DATA_VOLATILE; // Double check perf
-    texRange.RegisterSpace = 0;
-    texRange.RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
-
-    cbParams.emplace_back();
-    cbParams.back().InitAsDescriptorTable(1, &texRange, D3D12_SHADER_VISIBILITY_PIXEL);
-
-    CD3DX12_STATIC_SAMPLER_DESC linearClamp(
-        0,
-        D3D12_FILTER_MIN_MAG_MIP_LINEAR,
-        D3D12_TEXTURE_ADDRESS_MODE_CLAMP,
-        D3D12_TEXTURE_ADDRESS_MODE_CLAMP,
-        D3D12_TEXTURE_ADDRESS_MODE_CLAMP);
-    CD3DX12_STATIC_SAMPLER_DESC linearWrap(
-        1,
-        D3D12_FILTER_MIN_MAG_MIP_LINEAR,
-        D3D12_TEXTURE_ADDRESS_MODE_WRAP,
-        D3D12_TEXTURE_ADDRESS_MODE_WRAP,
-        D3D12_TEXTURE_ADDRESS_MODE_WRAP);
-    std::array<CD3DX12_STATIC_SAMPLER_DESC, 2> staticSamplers = { linearClamp, linearWrap };
-
-    D3D12_FEATURE_DATA_ROOT_SIGNATURE signatureData = {};
-    signatureData.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_1;
-
-    if (FAILED(context.Device->CheckFeatureSupport(D3D12_FEATURE_ROOT_SIGNATURE, &signatureData, sizeof(signatureData))))
-        signatureData.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_0;
-
-    D3D12_ROOT_SIGNATURE_FLAGS flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
-    CD3DX12_VERSIONED_ROOT_SIGNATURE_DESC rootSignatureDesc;
-    rootSignatureDesc.Init_1_1(UINT(cbParams.size()), cbParams.data(), UINT(staticSamplers.size()), staticSamplers.data(), flags);
-
-    Microsoft::WRL::ComPtr<ID3DBlob> signature;
-    Microsoft::WRL::ComPtr<ID3DBlob> rootSignatureCreationError;
-    HRESULT hr = D3DX12SerializeVersionedRootSignature(&rootSignatureDesc, signatureData.HighestVersion, &signature, &rootSignatureCreationError);
-    if (hr != S_OK)
-        OutputDebugStringA(reinterpret_cast<char*>(rootSignatureCreationError->GetBufferPointer()));
-    hr = context.Device->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&m_commonRootSig));
-
+    CreateCommonRootSignature(context.Device, IID_PPV_ARGS(&m_commonRootSig));
     NAME_D3D12_OBJECT(m_commonRootSig);
 }
 
@@ -196,31 +135,13 @@ void GltfViewer::CreatePSOs(RenderContext& context)
     Shader vs = Shader::CompileFromFile(shaderPath, "vs", "vs_5_1");
     Shader ps = Shader::CompileFromFile(shaderPath, "ps", "ps_5_1");
 
-    D3D12_GRAPHICS_PIPELINE_STATE_DESC desc = {};
-    desc.pRootSignature = m_commonRootSig.Get();
+    D3D12_GRAPHICS_PIPELINE_STATE_DESC desc = GetDefaultOpaquePsoDescriptor(m_commonRootSig.Get(), 1);
     desc.VS = vs.GetBytecode();
     desc.PS = ps.GetBytecode();
     desc.InputLayout = { inputLayout.data(), static_cast<UINT>(inputLayout.size()) };
-    desc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
-    desc.RasterizerState.CullMode = D3D12_CULL_MODE_BACK;
-    desc.RasterizerState.FillMode = D3D12_FILL_MODE_SOLID;
-    desc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
-
-    D3D12_DEPTH_STENCIL_DESC dsDesc = {};
-    dsDesc.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ALL;
-    dsDesc.DepthEnable = true;
-    dsDesc.StencilEnable = false;
-    dsDesc.DepthFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
-
-    desc.DepthStencilState = dsDesc;
-    desc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
 
     desc.DSVFormat = context.SwapChain->GetDepthStencilFormat();
-    desc.NumRenderTargets = 1;
     desc.RTVFormats[0] = m_tonemapper->GetHDRTargetFormat();
-
-    desc.SampleMask = UINT_MAX;
-    desc.SampleDesc.Count = 1;
 
     ThrowIfFailed(context.Device->CreateGraphicsPipelineState(&desc, IID_PPV_ARGS(&m_pso)));
 }
