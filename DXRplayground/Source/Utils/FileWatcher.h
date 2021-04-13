7311f49afe8f3ff621e10ccb374742ba364070d2
Aleksandr Vorontcov <nameless323@gmail.com>
2021-04-13 19:15:19 -0400

    More file watcher

--- DXRplayground/Source/Utils/FileWatcher.h
+++ DXRplayground/Source/Utils/FileWatcher.h
@@ -1,10 +1,14 @@
 #pragma once
 
+// Check https://qualapps.blogspot.com/2010/05/understanding-readdirectorychangesw.html to better understand this abomination.
+
 #include <atomic>
 #include <cassert>
 #include <string>
 #include <iostream>
 #include <thread>
+#include <atlstr.h>
+#include <shlwapi.h>
 
 class FileWatcher
 {
@@ -16,6 +20,7 @@ public:
     static constexpr DWORD m_bufferSize = 16384;
     std::vector<BYTE> m_buffer;
     std::vector<BYTE> m_backupBuffer;
+    CStringW m_wstrDirectory = L"C:\\Repos\\DXRplayground\\DXRplayground\\tmp";
 
     FileWatcher(std::string path)
         : m_path(std::move(path))
@@ -40,15 +45,59 @@ public:
             assert(false);
     }
 
+    void BackupBuffer(DWORD size)
+    {
+        memcpy(&m_backupBuffer[0], &m_buffer[0], size);
+    }
+
     void ReadDirectoryChanges()
     {
         DWORD dwBytes = 0;
-        BOOL sucess = ReadDirectoryChangesW(m_dirHandle, &m_buffer[0], m_buffer.size(), true, FILE_NOTIFY_CHANGE_LAST_WRITE, &dwBytes, &m_overlapped, &DirectoryModificationCallback);
+        BOOL sucess = ReadDirectoryChangesW(m_dirHandle, &m_buffer[0], DWORD(m_buffer.size()), true, FILE_NOTIFY_CHANGE_LAST_WRITE, &dwBytes, &m_overlapped, &DirectoryModificationCallback);
+    }
+
+    void ProcessDirectoryNotification()
+    {
+        BYTE* data = m_backupBuffer.data();
+        while (true) // Do some check with counter
+        {
+            FILE_NOTIFY_INFORMATION& fni = (FILE_NOTIFY_INFORMATION&)*data;
+
+            CStringW filename(fni.FileName, fni.FileNameLength / sizeof(wchar_t));
+            if (m_wstrDirectory.Right(1) != L"\\")
+                filename = m_wstrDirectory + L"\\" + filename;
+            else
+                filename = m_wstrDirectory + filename;
+
+            LPCWSTR wszFilename = PathFindFileNameW(filename);
+            int len = lstrlenW(wszFilename);
+
+            if (len <= 12 && wcschr(wszFilename, L'~'))
+            {
+                wchar_t wbuf[MAX_PATH];
+                if (GetLongPathNameW(filename, wbuf, _countof(wbuf)) > 0)
+                    filename = wbuf;
+            }
+            if (!fni.NextEntryOffset)
+                break;
+            data += fni.NextEntryOffset;
+        }
     }
 
     static VOID CALLBACK DirectoryModificationCallback(DWORD errorCode, DWORD numberOfBytesTransfered, LPOVERLAPPED overlapped)
     {
         FileWatcher* watcher = (FileWatcher*)overlapped->hEvent;
+
+        if (errorCode == ERROR_OPERATION_ABORTED)
+            return;
+
+        if (numberOfBytesTransfered == 0)
+            return;
+
+        watcher->BackupBuffer(numberOfBytesTransfered); // To avoid race condition.
+
+        watcher->ReadDirectoryChanges();
+        watcher->ProcessDirectoryNotification();
     }
 
     void operator()()
@@ -56,15 +105,15 @@ public:
         assert(m_isRunning && "File watcher thread is already running");
         m_isRunning = true;
 
+        ReadDirectoryChanges();
 
-
-
+        /*
         while (m_isRunning)
         {
             OutputDebugStringA("In the thread\n");
             std::this_thread::sleep_for(std::chrono::seconds(1));
         }
-        OutputDebugStringA("Thread stopped\n");
+        OutputDebugStringA("Thread stopped\n");*/
     }
 
     void Shutdown()
