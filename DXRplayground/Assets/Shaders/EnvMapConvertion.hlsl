6d66b0657604ef40bf27076da42ea14ef6fd02b4
Aleksandr Vorontcov <nameless323@gmail.com>
2021-10-28 21:30:04 -0400

    Correct store/load in EnvMapConvertion.hlsl

--- DXRplayground/Assets/Shaders/EnvMapConvertion.hlsl
+++ DXRplayground/Assets/Shaders/EnvMapConvertion.hlsl
@@ -11,10 +11,10 @@ ConstantBuffer<CbTIndices> cbTIndices : register(b0);
 [numthreads(32, 32, 1)]
 void cs(uint3 tId : SV_DispatchThreadID)
 {
-    cubemap[float3(tId.xy / 512.0f, 0)] = float4(1, 0, 0, 1);
-    cubemap[float3(tId.xy / 512.0f, 1)] = float4(0, 1, 0, 1);
-    cubemap[float3(tId.xy / 512.0f, 2)] = float4(0, 0, 1, 1);
-    cubemap[float3(tId.xy / 512.0f, 3)] = float4(1, 1, 0, 1);
-    cubemap[float3(tId.xy / 512.0f, 4)] = float4(0, 0, 0, 1);
-    cubemap[float3(tId.xy / 512.0f, 5)] = float4(1, 1, 1, 1);
+    cubemap[float3(tId.xy, 0)] = float4(1, 0, 0, 1);
+    cubemap[float3(tId.xy, 1)] = float4(0, 1, 0, 1);
+    cubemap[float3(tId.xy, 2)] = float4(0, 0, 1, 1);
+    cubemap[float3(tId.xy, 3)] = float4(1, 1, 0, 1);
+    cubemap[float3(tId.xy, 4)] = float4(0, 0, 0, 1);
+    cubemap[float3(tId.xy, 5)] = float4(1, 1, 1, 1);
 }
