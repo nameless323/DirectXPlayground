b073a8f41c883ab8baf35577575061c9a5ac2d31
Aleksandr Vorontcov <nameless323@gmail.com>
2022-01-18 18:40:26 -0500

    EnvMapConvertion.hlsl fixed seams, removed junk

--- DXRplayground/Assets/Shaders/EnvMapConvertion.hlsl
+++ DXRplayground/Assets/Shaders/EnvMapConvertion.hlsl
@@ -2,6 +2,7 @@ RWTexture2DArray<float4> cubemap : register(u0);
 Texture2D<float4> envMap : register(t0);
 
 #define PI 3.14159265358979323846f
+#define PI_2 (PI / 2.0f)
 
 SamplerState LinearClampSampler : register(s0);
 
@@ -11,12 +12,12 @@ struct CbData
 };
 ConstantBuffer<CbData> cbData : register(b0);
 
-float3 OutImgToXYZ(uint i, uint j, uint face, uint edge)
+float3 OutImgToXYZ(uint i, uint j, uint face, uint2 edge)
 {
-    float a = 2.0f * float(i) / float(edge) - 1.0f;
-    float b = 2.0f * float(j) / float(edge) - 1.0f;
+    float a = 2.0f * float(i) / float(edge.x) - 1.0f;
+    float b = 2.0f * float(j) / float(edge.y) - 1.0f;
     float3 res;
-    if (face == 0) // back
+    if (face == 0)
         res = float3(-1.0f, -a, -b);
     else if (face == 1)
         res = float3(a, -1.0f, -b);
@@ -34,39 +35,13 @@ float3 OutImgToXYZ(uint i, uint j, uint face, uint edge)
 [numthreads(32, 32, 1)]
 void cs(uint3 tId : SV_DispatchThreadID)
 {
-    uint2 inSize = cbData.eqMapCubeMapWH.xy;
-    uint2 edge = cbData.eqMapCubeMapWH.zw;
-    uint face = uint(tId.z);
-    float3 coord = OutImgToXYZ(tId.x, tId.y, face, edge);
+    uint2 eqMapSize = cbData.eqMapCubeMapWH.xy;
+    float3 coord = OutImgToXYZ(tId.x, tId.y, uint(tId.z), cbData.eqMapCubeMapWH.zw);
+
     float theta = atan2(coord.y, coord.x);
     float r = length(coord.xy);
     float phi = atan2(coord.z, r);
 
-    float uf = 2.0f * edge.x * (theta + PI) / PI;
-    float vf = 2.0f * edge.y * (PI / 2.0f - phi) / PI;
-    float2 uv = float2(uf % inSize.x, clamp(vf, 0.0f, inSize.y - 1.0f));
-    uv /= inSize.xy;
-    cubemap[float3(tId.xyz)] = envMap.Sample(LinearClampSampler, uv);
-/*
-    uint u1 = floor(uf);
-    uint v1 = floor(vf);
-    uint u2 = u1 + 1;
-    uint v2 = v1 + 1;
-    float mu = uf - u1;
-    float nu = vf - v1;
-
-    float4 a = envMap.Load(float2(u1 % inSize.x, clamp(v1, 0, inSize.y - 1))); // don't need this shit. sample uf vf directly
-    float4 b = envMap.Load(float2(u2 % inSize.x, clamp(v1, 0, inSize.y - 1)));
-    float4 c = envMap.Load(float2(u1 % inSize.x, clamp(v2, 0, inSize.y - 1)));
-    float4 d = envMap.Load(float2(u2 % inSize.x, clamp(v2, 0, inSize.y - 1)));
-
-    float4 lh = lerp(a, b, mu);
-    float4 ll = lerp(c, d, mu);
-    float4 res = lerp(lh, ll, nu);
-
-    //for (uint i = 0; i < iterCnt; ++i)
-    {
-        float2 uv = tId.xy / cbData.eqMapCubeMapWH.zw;
-        cubemap[float3(tId.xyz)] = envMap.Sample(LinearClampSampler, uv);
-    }*/
+    float2 uv = float2((theta + PI) / PI * 0.5f, (PI_2 - phi) / PI);
+    cubemap[float3(tId.xyz)] = envMap.SampleLevel(LinearClampSampler, uv, 0);
 }
