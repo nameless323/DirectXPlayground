eefa52f0f273c98dfabf911db4de0e996e4fa097
Aleksandr Vorontcov <nameless323@gmail.com>
2022-04-06 19:55:48 -0400

    BinaryContainer more stream operators

--- DXRplayground/Source/Utils/BinaryContainer.h
+++ DXRplayground/Source/Utils/BinaryContainer.h
@@ -26,8 +26,10 @@ private:
 
 public:
     BinaryContainer& operator<< (int val);
+    BinaryContainer& operator<< (size_t val);
     BinaryContainer& operator<< (std::vector<int> val);
     BinaryContainer& operator<< (std::string val);
+    BinaryContainer& operator<< (std::pair<unsigned char*, size_t> val);
 };
 
 inline BinaryContainer::BinaryContainer(size_t initialCapacity, std::function<size_t(size_t)> resizePolicy)
@@ -43,5 +45,60 @@ inline BinaryContainer::~BinaryContainer()
     delete[] mData;
 }
 
+inline void BinaryContainer::Resize()
+{
+    mCapacity = mResizePolicy(mCapacity);
+    unsigned char* tmp = new unsigned char[mCapacity];
+    memcpy(tmp, mData, mBytesWritten);
+    std::swap(tmp, mData);
+    delete[] tmp;
+}
 
+inline BinaryContainer& BinaryContainer::operator<<(int val)
+{
+    if (sizeof(val) + mBytesWritten > mCapacity)
+        Resize();
+    unsigned char* p = mData + mBytesWritten;
+    mBytesWritten += sizeof(val);
+    memcpy(p, &val, sizeof(val));
+    return *this;
+}
+
+inline BinaryContainer& BinaryContainer::operator<<(size_t val)
+{
+    while (sizeof(val) + mBytesWritten > mCapacity)
+    {
+        Resize();
+    }
+    unsigned char* p = mData + mBytesWritten;
+    mBytesWritten += sizeof(val);
+    memcpy(p, &val, sizeof(val));
+    return *this;
+}
+
+inline BinaryContainer& BinaryContainer::operator<<(std::vector<int> val)
+{
+    while (sizeof(int) * val.size() + sizeof(size_t) + mBytesWritten > mCapacity)
+    {
+        Resize();
+    }
+    this->operator<<(val.size());
+    unsigned char* p = mData + mBytesWritten;
+    mBytesWritten += sizeof(int) * val.size();
+    memcpy(p, val.data(), sizeof(int) * val.size());
+    return *this;
+}
+
+inline BinaryContainer& BinaryContainer::operator<<(std::string val)
+{
+    while (val.size() + sizeof(size_t) + mBytesWritten > mCapacity)
+    {
+        Resize();
+    }
+    this->operator<<(val.size());
+    unsigned char* p = mData + mBytesWritten;
+    mBytesWritten += val.size();
+    memcpy(p, val.data(), val.size());
+    return *this;
+}
 }
